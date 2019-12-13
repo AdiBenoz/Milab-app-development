dfd75a0149d81fa38e75d9576d53dca651f00bd4
adi <adi@benoz.net>
2019-12-13 19:09:52 +0200

    working version - not jumping + ugly 2

--- EX03/app/src/main/java/com/example/ex03/NotifyIntentService.java
+++ EX03/app/src/main/java/com/example/ex03/NotifyIntentService.java
@@ -7,13 +7,6 @@ import android.content.Intent;
 import android.content.Context;
 import android.util.Log;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
 public class NotifyIntentService extends IntentService {
     private static final String ACTION_BUMP_QUOTE = "com.example.exercise_03.action.BUMP_QUOTE";
 
